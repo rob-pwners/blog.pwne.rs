7d19fecf6842b0659b4ab7a093116ea616832a79
Rob W <41654240+rob-pwners@users.noreply.github.com>
2019-06-06 22:42:18 +0100

    Updated theme

--- themes/hello-friend/webpack.config.js
+++ themes/hello-friend/webpack.config.js
@@ -87,22 +87,9 @@ module.exports = {
     },
     minimizer: [
       new UglifyJsPlugin({
-        sourceMap: false,
+        sourceMap: true,
       }),
     ],
   },
-  plugins: [
-    new CleanPlugin(join("static/assets")),
-    new HtmlWebpackPlugin({
-      template: join("source", "html", "inject.script.ejs"),
-      filename: join("layouts", "partials", "inject.script.html"),
-      inject: false,
-    }),
-    new HtmlWebpackPlugin({
-      template: join("source", "html", "inject.stylesheet.ejs"),
-      filename: join("layouts", "partials", "inject.stylesheet.html"),
-      inject: false,
-    }),
-    new ExtractTextPlugin("[name]"),
-  ],
+  plugins: [new CleanPlugin(join("static/assets")), new ExtractTextPlugin("[name]")],
 };
